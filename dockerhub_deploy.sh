d50d21689d9d2234a63a72333b01616512944770
Michael van Vliet <info@michaelvanvliet.nl>
2017-09-08 14:21:23 +0200

    removed Travis build system, switched to DockerHub automated builds

--- dockerhub_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Dockerhub login...'
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-
-echo 'Dockerhub push...'
-docker push michaelvanvliet/container-data-analysis:latest
-echo 'Finished!'
