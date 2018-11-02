da1f932b3ebff03b580011b2a5218a03098eb36f
Michael van Vliet <info@michaelvanvliet.nl>
2018-11-02 23:27:09 +0100

    switched from a Conda installation of R to a default CentOS installation for both Jupyter and Rstudio.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 if [ -x "$(command -v docker)" ]; then
     echo "Found Docker... preparing container"
     docker rm jhda # clean previous version
-    docker run -it --name jhda -p 8787:8787 -p 8000:8000 -p 8001:8001 jh-da /bin/bash -c "rstudio-server start; /tmp/anaconda3/bin/jupyterhub jupyterhub -f /etc/jupyterhub/jupyterhub_config.py --no-ssl"
+    docker run -it --name jhda -p 8787:8787 -p 8000:8000 -p 8001:8001 jh-da-base-image /bin/bash -c "rstudio-server start; /tmp/anaconda3/bin/jupyterhub jupyterhub -f /etc/jupyterhub/jupyterhub_config.py --no-ssl"
 else
     echo "Please install Docker: https://docs.docker.com/install/"
 fi
